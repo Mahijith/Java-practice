019c34b4cb68386a7c3d3395ef88b77ca75119a7
Windixers <84848576+Mahijith@users.noreply.github.com>
2022-11-22 21:42:20 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,18 @@
+
+//User function Template for Java
+
+
+class Solution
+{
+    public void utility(int a, int b){
+        //code here
+        //a=c;
+        int c=0;
+        c=a;
+        a=b;
+        b=c;
+        
+        
+        System.out.println(a+" "+b);
+    }
+}
